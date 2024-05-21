5584679886ea9fac655fcba816b2aed35c93ca31
sabinabakirzada <bd6zz1ghz@code.edu.az>
2024-05-21 08:01:25 -0700

    modified favs

--- 03.Example/Client/src/pages/Favorite/index.jsx
+++ 03.Example/Client/src/pages/Favorite/index.jsx
@@ -1,30 +1,59 @@
-import React, { useContext } from 'react'
-import { FavsContext } from '../../context/favsContext';
+import React, { useContext } from "react";
+import { FavsContext } from "../../context/favsContext";
 import style from "./index.module.scss";
+import { Table } from "antd";
 
 const Favorite = () => {
-    const { favs, setFavs, setLocalFavs } = useContext(FavsContext);
-    console.log(favs)
+  const { favs, setFavs, setLocalFavs } = useContext(FavsContext);
+  console.log(favs);
+  const columns = [
+    {
+      title: "Title",
+      dataIndex: "title",
+    },
+    {
+      title: "Image",
+      render: (record) => {
+        return <img src={record.img} alt="" width={"100px"} />;
+      },
+    },
+    {
+      title: "Price",
+      dataIndex: "price",
+    },
+     {
+      title: "Reset",
+      render: (record) => {
+        return (
+          <button
+            onClick={() => {
+              const updated = favs.filter((q) => q._id !== record._id);
+              setFavs(updated);
+              setLocalFavs(updated);
+            }}
+          >
+            Reset
+          </button>
+        );
+      },
+    },
+  ];
+    const onChange = (pagination, filters, sorter, extra) => {
+    console.log("params", pagination, filters, sorter, extra);
+  };
   return (
-<>
-   {favs && favs.map((product)=>{
-    return (
-          <div className={style.card}>
-          <div className={style.cardImg}>
-            <img src={product.img} alt="" />
-          </div>
-          <div className={style.cardTitle}>
-            <h3>
-              <b>${product.price}.50</b>
-            </h3>
-            <h2>{product.title}</h2>
-            <p>{product.description}</p>
-          </div>
-        </div>
-    )
-   })}
-</>
-  )
-}
+    <>
+      <Table
+        style={{ marginTop: "60px" }}
+        columns={columns}
+        dataSource={favs}
+        onChange={onChange}
+        showSorterTooltip={{
+          target: "sorter-icon",
+        }}
+      />
+    </>
+  );
+};
 
-export default Favorite
+export default Favorite;
